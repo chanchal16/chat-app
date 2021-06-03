fd481d227270aafe346588d3056ea94f42a0d02d
chanchal16 <rajputchanchal17@gmail.com>
2021-06-03 15:46:42 +0530

    update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,7 +9,9 @@ import {auth} from '../config'
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider);
-      }
+    }
+
+    
     return (
         <div>
              <div className="card">
@@ -17,6 +19,7 @@ import {auth} from '../config'
                     <h3><b>One Time Sign In</b></h3>
                     <p style={{color:'black'}}>Sign in with your google account to get started.</p>
                     <button className='signin-btn' onClick={signInWithGoogle}>Sign in with Google</button>
+                    <button className='guest' onClick={loginGuest}>Login as Guest</button>
                 </div>
             </div>
         </div>
